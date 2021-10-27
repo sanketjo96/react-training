a5be8f1f39c4132d06dd7edb0a6ce168902520a9
sanket joshi <sanket@sankets-MacBook-Pro.local>
2021-10-27 15:44:32 +0530

    effect without pareams

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+// import { useEffect } from "react";
+
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // Infi loop
+  // if (localStorage.getItem("isLoggedIn", 1) === "1") {
+  //   setIsLoggedIn(true);
+  // }
+
+  // Usage of effect hook
+  // useEffect(() => {
+  //   if (localStorage.getItem("isLoggedIn", 1) === "1") {
+  //     setIsLoggedIn(true);
+  //   }
+  // }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    /**
+     * Post login refresh throws user out of the app
+     * we need a functionality where post refresh state needs to be maintianed
+     */
+
+    localStorage.setItem("isLoggedIn", 1);
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.clear("isLoggedIn");
     setIsLoggedIn(false);
   };
 
